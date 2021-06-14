e3ea05435dfe544ff7a0fef055c308b0d27bc1e1
Serah wakaba <serahwakaba9@gmail.com>
2021-06-14 10:58:35 +0300

    files commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,7 @@
 //
 //
 //        get("/",(request, response) ->
-//        {
+   {
 //                Map<String, Object>model=new HashMap<String, Object>();
 //                List<EndangeredAnimals> endangeredAnimals=EndangeredAnimals.getAll();
 //                model.put("endangeredAnimals", endangeredAnimals);
