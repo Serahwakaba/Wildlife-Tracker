e0e0dc2c3d20d369951c54d96fe58e0b06b801e3
Serah wakaba <serahwakaba9@gmail.com>
2021-06-14 11:55:59 +0300

    updated database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,19 +8,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//import static spark.Spark.*;
-//public class App {static int getHerokuAssignedPort() {
-//    ProcessBuilder processBuilder = new ProcessBuilder();
-//    if (processBuilder.environment().get("PORT") != null) {
-//        return Integer.parseInt(processBuilder.environment().get("PORT"));
-//    }
-//    return 4567;
-//}
-//    public static void main(String[] args) {
-//        port(getHerokuAssignedPort());
-//        staticFileLocation("/public");
-//
-//
+import static spark.Spark.*;
+public class App {static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567;
+}
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+
         get("/",(request, response) ->
    {
                Map<String, Object>model=new HashMap<String, Object>();
--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -2,8 +2,11 @@ package models;
 import org.sql2o.*;
 public class Database {
 
-    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-34-195-169-25.compute-1.amazonaws.com:5432/d3elr8g0ab9mu8", "eayrbquvvrizmd", "11cdc47f2d2b42fbd67ff82ec73dcef70a5e2d13af20152e2f2c2bb306fd3c53");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-35-169-188-58.compute-1.amazonaws.com:5432/d2gvrj136a2r78", "ynkxmmwxgxmsqu", "33fbd2b96bbdd6a58c359e703e7bace66e398327c5d445e5cc58c7023681afb2");
 
-   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "12345");
+//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "12345");
+
+//   postgres://ynkxmmwxgxmsqu:33fbd2b96bbdd6a58c359e703e7bace66e398327c5d445e5cc58c7023681afb2@
+//   // ec2-35-169-188-58.compute-1.amazonaws.com:5432/d2gvrj136a2r78
 
 }
